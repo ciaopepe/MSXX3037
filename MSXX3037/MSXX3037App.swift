2ca177ba8c1a9499101f90536748af35922a1b55
ciaopepe <guhatto@gmail.com>
2026-03-09 17:54:27 +0900

    Initial Commit

---
+++ MSXX3037/MSXX3037App.swift
@@ -0,0 +1,17 @@
+//
+//  MSXX3037App.swift
+//  MSXX3037
+//
+//  Created by KMatsui on 2026/03/09.
+//
+
+import SwiftUI
+
+@main
+struct MSXX3037App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MSXX3037Tests/MSXX3037Tests.swift
@@ -0,0 +1,17 @@
+//
+//  MSXX3037Tests.swift
+//  MSXX3037Tests
+//
+//  Created by KMatsui on 2026/03/09.
+//
+
+import Testing
+@testable import MSXX3037
+
+struct MSXX3037Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
